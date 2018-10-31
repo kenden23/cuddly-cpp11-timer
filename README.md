5c8bd8c6a270a03306f3086767e49ff7ecfb9267
BillSu <35948036@qq.com>
2018-10-31 16:10:56 +0800

    1. more concise code 2. more convenient functions

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ a simple and head only timer, written with cpp11 standard, suitable for game and
 
 ## usage
 just #include the Timer.h, and you are good to go.
+
+2018-10-31 update:
+1. more concise code, using cpp template to reuse code, make the code real small.
+2. add more function: get millionsecond, microsecond, nanosecond.
--- Timer.h
+++ Timer.h
@@ -32,6 +32,9 @@ public:
 	Timer(){}
 	~Timer(){}
 	
+	/**
+	 *	
+	 */
 	template<typename T = float>
 	T getTime()
 	{
@@ -46,33 +49,71 @@ public:
 		start_time = std::chrono::steady_clock::now();
 	}
 
-	// @return: time last from setStartPoint() till now, don't update the start time point
-	template<typename T = float>
-	T getTimeSpan()
+private:
+	template<typename T>
+	T timeSpanTemp(bool updateTime = false)
 	{
 		using namespace std::chrono;
 		auto end_time = steady_clock::now();
 		auto sp = end_time - start_time;
-		duration<T> t = duration_cast<duration<T>>(sp);
+		auto t = duration_cast<T>(sp);
+		if (updateTime) start_time = end_time;
 
 		// Force nonnegative.  The DXSDK's CDXUTTimer mentions that if the 
 		// processor goes into a power save mode or we get shuffled to another
 		// processor, then mDeltaTime can be negative.
 		// refer : Introduction to 3D Game Programming With DirectX11, but will it happen with c++?
 		// return t.count() < 0 ? 0 : t.count();
-		return t.count();
+		return t;
+	}
+
+public:
+	// @return: time last from setStartPoint() till now, don't update the start time point
+	// @return: second unit for :float and double
+	template<typename T = float>
+	T getTimeSpan()
+	{
+		return timeSpanTemp<std::chrono::duration<T>>().count();
+	}
+
+	// 1:1000
+	long long getTimeSpanMilliseconds()
+	{
+		return timeSpanTemp<std::chrono::milliseconds>().count();
+	}
+	// 1:1000000
+	long long getTimeSpanMicroSeconds()
+	{
+		return timeSpanTemp<std::chrono::microseconds>().count();
+	}
+	// 1:1000000000
+	long long getTimeSpanNanoSeconds()
+	{
+		return timeSpanTemp<std::chrono::nanoseconds>().count();
 	}
 
 	// @return: time last from setStartPoint() till now, update the start time point
+	// @return: second unit for :float and double
 	template<typename T = float>
 	T updateTimeSpan()
 	{
-		using namespace std::chrono;
-		auto end_time = steady_clock::now();
-		auto sp = end_time - start_time;
-		duration<T> t = duration_cast<duration<T>>(sp);
-		start_time = end_time;
-		return t.count();
+		return timeSpanTemp<std::chrono::duration<T>>(true).count();
+	}
+
+	// 1:1000
+	long long updateTimeSpanMilliseconds()
+	{
+		return timeSpanTemp<std::chrono::milliseconds>(true).count();
+	}
+	// 1:1000000
+	long long updateTimeSpanMicroSeconds()
+	{
+		return timeSpanTemp<std::chrono::microseconds>(true).count();
+	}
+	// 1:1000000000
+	long long updateTimeSpanNanoSeconds()
+	{
+		return timeSpanTemp<std::chrono::nanoseconds>(true).count();
 	}
 
 private:
